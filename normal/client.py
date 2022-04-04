2c42200349bbd23537e80abb8d26bfe7f6de42ee
jesvarzam <jesvarzam@alum.us.es>
2022-04-04 19:50:10 +0200

    Minor changes in order to clean code

--- normal/client.py
+++ normal/client.py
@@ -3,13 +3,14 @@ import socket, ssl
 HOST, PORT = '127.0.0.1', 443
 
 def send_message(conn):
-    user = input("\n Enter your username: ")
-    password = input("\n Enter your password: ")
-    message = input("\n Enter the message you want to send: ")
+	
+    user = input("\nEnter your username: ")
+    password = input("\nEnter your password: ")
+    message = input("\nEnter the message you want to send: ")
 
     total_message = user + ":" + password + ":" + message
     conn.send(total_message.encode())
-    print(conn.recv().decode())
+    print(conn.recv(1024).decode())
 
 def main():
 
@@ -18,13 +19,14 @@ def main():
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     context.check_hostname = False
     context.verify_mode=ssl.CERT_NONE
-
-    #context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 
     context.set_ciphers('AES256+SHA384')
+
     conn = context.wrap_socket(sock, server_hostname=HOST)
 
     try:
         conn.connect((HOST, PORT))
+        check_conn = conn.recv(1024)
+        print('\n' + check_conn.decode())
         send_message(conn)
     finally:
         conn.close()
--- normal/server.py
+++ normal/server.py
@@ -1,15 +1,14 @@
 import socket, ssl, os
 
-CERT_PATH = os.path.dirname(os.path.abspath(__file__)) + "../utils/mycert.pem"
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-context.load_cert_chain(certfile=CERT_PATH) 
+CERT_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../utils/mycert.pem"
 
 def recv_message(conn):
-    conn.recv(1024)
+    message = conn.recv(1024)
+    print('\n[*] Message received: ' + message.decode())
     conn.send(b'\n[+] Message received!')
 
-while True:
+if __name__ == '__main__':
+  
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
   sock.bind(('', 443))
@@ -17,15 +16,16 @@ while True:
   sock.listen(5)
 
   context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-  context.load_cert_chain(certfile="mycert.pem") 
+  context.load_cert_chain(certfile=CERT_PATH) 
   context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2 | ssl.OP_NO_COMPRESSION
   context.set_ciphers('AES256+SHA384')
   
   while True:
     client, addr = sock.accept()
-    print('[+] Connection accepted!')
+    print('\n[+] Connection received from {}'.format(addr))
     try:
       conn = context.wrap_socket(client, server_side=True)
+      conn.send(b'[+] Connection accepted!')
       recv_message(conn)
     except ssl.SSLError as e:
       print(e)
